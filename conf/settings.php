d6f237e34a6bb4a13dd88cade0e0969e31b2d441
IonutMuresan <plexaz.ionut@gmail.com>
2020-05-10 18:36:01 +0300

    Added final favicon.ico

--- conf/settings.php
+++ conf/settings.php
@@ -1,6 +1,15 @@
 <?php
 
-$servername = "localhost";
-$username = "olympicsports_user";
-$password = "olympicsports_password";
-$dbname = "olympicsports";
+$local_settings_file_path="/";
+$local_settings_file_name="local_settings.php";
+
+if(file_exists($local_settings_file_path.$local_settings_file_name)) {
+    include $local_settings_file_path.$local_settings_file_name;
+}else {
+	//Define default Database connection
+	$servername = "localhost";
+	$username 	= "olympicsports_user";
+	$password 	= "olympicsports_password";
+	$dbname 	= "olympicsports";
+}
+?>
